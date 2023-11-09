5034260f3b7badda8088ce8b2ae4348552991551
Sean <seanhia15@gmail.com>
2023-11-08 21:05:08 -0800

    lettergrade update

--- LetterGrade.py
+++ LetterGrade.py
@@ -1,13 +1,16 @@
 from orm_base import Base
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 from Enrollment import Enrollment
 
-class LetterGrade(Base):
+class LetterGrade(Enrollment):
     __tablename__ = "letter_grade"
     minSatisfactory: Mapped[str] = mapped_column("min_satisfactory", String(10), nullable=False)
+    letterGrade: Mapped[str] = mapped_column("letter_grade",  ForeignKey("enrollments.enrollment_id", ondelete="CASCADE"), primary_key=True)
 
-
+    __mapper_args__ = {"polymorphic_identity":"letter_grade"}
+    __table_args__ = (UniqueConstraint("letter_grade",
+                                       name="letterGrade_uk_01"))
 
     def __init__(self, section, student, minSatisfactory: str):
         super().__init__(section, student)
